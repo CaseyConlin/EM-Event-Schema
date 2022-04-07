fc7d7ec7281d966d72ae7c7e0f07ff29c55c7a0d
CaseyConlin <101153261+CaseyConlin@users.noreply.github.com>
2022-04-07 17:59:27 -0400

    Update functions file to remove WP Hooks
    Move calls to DB to the top outside JSON code.

--- includes/eschemaem-functions.php
+++ includes/eschemaem-functions.php
@@ -1,58 +1,115 @@
 <?php
-function e_shchema_em(){
-
-	
-if (is_singular('event')) {?>
-<script type="application/ld+json">
- [{
-    "@context": "http://schema.org",
- 	"@type": "<?php echo get_post_meta(get_the_ID(), 'Event Type', true); ?>",
-
-    "name": "<?php the_title (); ?>",
-    "description": "<?php echo(get_the_excerpt()) ?>",
- 	"image": "<?php the_post_thumbnail_url(); ?>",
- 	"url": "<?php echo get_permalink(); ?>",
-    "startDate": "<?php echo get_post_meta(get_the_ID(), '_event_start_date', true);echo " "; echo get_post_meta(get_the_ID(), '_event_start_time', true); ?>",
- 	"endDate": "<?php echo get_post_meta(get_the_ID(), '_event_end_date', true);echo " "; echo get_post_meta(get_the_ID(), '_event_end_time', true); ?>",
-"location": {
-    <?php 
-	global $wpdb;
-	$locationId = get_post_meta(get_the_ID(), '_location_id', true); ?>
-	  "@type": "Place",
-    "name": "<?php  $locationName = $wpdb->get_var('SELECT location_name FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationName; ?>",
-    "description": "<?php  $locationDesc = $wpdb->get_var('SELECT post_content FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationDesc; ?>",
-	"url": "<?php echo home_url(); ?> ",
-    "address":{
-		"@type": "PostalAddress",
-		"streetAddress": "<?php  $locationStreet = $wpdb->get_var('SELECT location_address FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationStreet; ?>",
-		"addressLocality": "<?php  $locationTown = $wpdb->get_var('SELECT location_town FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationTown; ?>",
- 		"addressRegion":  "<?php  $locationState = $wpdb->get_var('SELECT location_state FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationState; ?>",
-		"postalCode":  "<?php  $locationZip = $wpdb->get_var('SELECT location_postcode FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo $locationZip; ?>",
-		"addressCountry": "United States",
-		"telephone": "<?php $locationPostid = $wpdb->get_var('SELECT post_id FROM '.$wpdb->prefix.'em_locations WHERE location_id ='.$locationId); echo get_post_meta($locationPostid, "phoneNumber", true); ?>",
-		"sameAs": "<?php echo home_url(); ?>"
+function e_schema_em1 () {
+	/*see if the post is an event and add schema if true*/
+if (is_singular( 'event' )){
+  e_shchema_em();
 }
-}, 	
-<?php $performer = get_post_meta(get_the_ID(), 'Artist Name', true); 
-                         if($performer){ ?>
-    "performer": {
-        "@type": "PerformingGroup",
- 		"name": "<?php echo get_post_meta(get_the_ID(), 'Artist Name', true); ?>",
- 		"sameAs": "<?php echo get_post_meta(get_the_ID(), 'Artist Website', true); ?>"
- 	},
-<?php }; ?>
+};
+/*find data for schema and create arrays*/
+function e_shchema_em(){
+	global $wpdb;
+	/*get some id info for later*/ 
+	  $postId = get_the_ID();
+	  $eventId = $wpdb->get_var($wpdb->prepare("SELECT event_id FROM {$wpdb->prefix}em_events WHERE post_id = %d", $postId ) );
+	  $elocationId = get_post_meta($postId, '_location_id', true);
+	  $elocationPostid = $wpdb->get_var($wpdb->prepare("SELECT post_id FROM {$wpdb->prefix}em_locations WHERE location_id = %d", $elocationId ) );
+	/*grab data with wp hooks*/
+	  $eventTitle = get_the_title(); 
+	  $eventExcerpt = get_the_excerpt();
+	  $eventImage = get_the_post_thumbnail_url();
+	  $eventUrl = get_permalink();
+	  $url = home_url();
+	  /*meta info with post Id*/
+	  $eventType = get_post_meta($postId, 'Event Type', true); 
+	  $ePerformer = get_post_meta($postId, 'Artist Name', true);
+	  $eperfWeb	= get_post_meta($postId, 'Artist Website', true);
+	  $eventStart =  get_post_meta($postId, '_event_start_date', true)." ".get_post_meta($postId, '_event_start_time', true);
+	  $eventEnd =  get_post_meta($postId, '_event_end_date', true)." ".get_post_meta($postId, '_event_end_time', true);
+	  /*meta info with location Id*/
+	  $eventPhone = get_post_meta($elocationPostid, 'phoneNumber', true); 
+	  /*grab all location info from Events Manager table */
+	  $eLocationinfo = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}em_locations WHERE location_id = %d", $elocationId ) ); 
+      /* grab info from ticket table*/
+	  $eTicketinfo = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}em_tickets WHERE event_id = %d", $eventId) ); 
+//
+  $eaSinfo = array(
+	  "@context"=> "http://schema.org",
+	  "@type"=> $eventType,
+	  "name" => $eventTitle,
+	  "description" => $eventExcerpt,
+	  "image" => $eventImage,
+	  "url"=> $eventUrl,
+	  "startDate"=> $eventStart,
+	  "endDate"=> $eventEnd,
+	  "location"=> array (
+		"@type"=> "Place",
+		"name"=> $eLocationinfo->location_name,
+		"description"=> $eLocationinfo->post_content,
+		"url"=> $url,
+		  "address"=> array(
+		    "@type"=> "PostalAddress",
+		    "streetAddress"=> $eLocationinfo->location_address,
+		    "addressLocality"=> $eLocationinfo->location_town,
+		    "postalCode"=> $eLocationinfo->location_postcode,
+		    "addressCountry"=> "United States",
+		    "telephone"=> $eventPhone,
+		    "sameAs"=> $url,
+		    ),
+	     ),
+	  "offers"=> array(
+ 	    "@type"=> "Offer",
+ 		"priceCurrency"=> "USD",
+		"price" =>  number_format($eTicketinfo->ticket_price, 2),
+		"validFrom"=> $eTicketinfo->ticket_start,
+		"url"=> $eventUrl,
+	  )
+  );
+  $eaSinfop = array(
+	  "@context"=> "http://schema.org",
+	  "@type"=> $eventType,
+	  "name" => $eventTitle,
+	  "description" => $eventExcerpt,
+	  "image" => $eventImage,
+	  "url"=> $eventUrl,
+	  "startDate"=> $eventStart,
+	  "endDate"=> $eventEnd,
+	  "location"=> array (
+		"@type"=> "Place",
+		"name"=> $eLocationinfo->location_name,
+		"description"=> $eLocationinfo->post_content,
+		"url"=> $url,
+		  "address"=> array(
+		    "@type"=> "PostalAddress",
+		    "streetAddress"=> $eLocationinfo->location_address,
+		    "addressLocality"=> $eLocationinfo->location_town,
+		    "postalCode"=> $eLocationinfo->location_postcode,
+		    "addressCountry"=> "United States",
+		    "telephone"=> $eventPhone,
+		    "sameAs"=> $url,
+		    ),
+	     ),
+	  "performer"=> array(
+        "@type" =>"PerformingGroup",
+ 		"name"=> $ePerformer,
+		"sameAs"=> $eperfWeb,
 
- 	"offers": {
- 		"@type": "Offer",
- 		"priceCurrency": "USD",
-		<?php $eventId = $wpdb->get_var('SELECT event_id FROM '.$wpdb->prefix.'em_events WHERE post_id ='.get_the_ID()); ?>
-		"price" : "<?php $ticketPrice = $wpdb->get_var('SELECT ticket_price FROM '.$wpdb->prefix.'em_tickets WHERE event_id ='.$eventId); if($ticketPrice){ echo (float)$ticketPrice;} else { echo "0";} ?>",
-<?php $ticketStart = $wpdb->get_var('SELECT ticket_start FROM '.$wpdb->prefix.'em_tickets WHERE event_id ='.$eventId); ?>
-		<?php if($ticketStart){ ?>"validFrom" : "<?php echo $ticketStart; ?>",
-<?php } ?>
-		"url": "<?php echo get_permalink(); ?>"
-
- 	}
- }]
+	  ),
+	  "offers"=> array(
+ 	    "@type"=> "Offer",
+ 		"priceCurrency"=> "USD",
+		"price" =>  number_format($eTicketinfo->ticket_price, 2),
+		"validFrom"=> $eTicketinfo->ticket_start,
+		"url"=> $eventUrl,
+	  )
+  );
+  ?>
+  <script type="application/ld+json">
+ <?php
+ /*output one array as json for schema based on $ePerformer variable*/
+	if($ePerformer!=""){ echo json_encode($eaSinfop,JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);}
+      else {echo json_encode($eaSinfo,JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);} 
+?>
 </script>
-<?php  }} ?>
+<?php
+}
+?>
